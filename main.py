dcbbab454a3ab15f9d15c89a4d6d5fd0949f8d73
GyeongSik Son <apple06521@gmail.com>
2024-09-19 01:16:22 +0900

    Develop (~9.10)

--- main.py
+++ main.py
@@ -3,12 +3,27 @@ from summary import *
 
 
 async def root():
+
     print("Please Input Youtube Code : ")
     url = input()
+
+    print("Please Input Summary Mode (1: Normal, 2: Compress")
+    summary_mode = int(input())
+
+    print("Please Input Divide Mode (1: Naive, 2:RCTS, 3:SPACY) : ")
+    mode = int(input())
+
     script = Script()
     get_script(script, url)
-    print("script = " + script.text)
-    summary, summary_list = await generate_summary(script.raw_script, '')
+
+    print(script.text)
+
+    if summary_mode == 1:
+        summary = await generate_summary(script.text, mode)
+
+    elif summary_mode == 2:
+        summary = await generate_compress_summary(script.text, mode)
+
     print(summary)
 
 if __name__ == '__main__':
--- summary.py
+++ summary.py
@@ -1,11 +1,19 @@
-import datetime
-
-from config import constants
+from langchain_text_splitters import RecursiveCharacterTextSplitter, SpacyTextSplitter
+import textwrap
 from gpt import *
 
 
-async def generate_summary(scripts: dict, video_title: str):
-    time_stamp, chunks = naive_divide(scripts)
+async def generate_summary(script: str, mode: int):
+
+    chunks = []
+
+    if mode == 1:
+        chunks = naive_divide(script)
+    elif mode == 2:
+        chunks = recursive_divide(script)
+    elif mode == 3:
+        chunks = spacy_divide(script)
+
     summary_prompt = constants['prompt']['final_summary']['kr']
 
     tasks = [request_gpt(summary_prompt + "\n script : " + chunk,
@@ -18,36 +26,73 @@ async def generate_summary(scripts: dict, video_title: str):
         final_summary += summary + '\n \n '
 
     final_summary = reformat_summary(final_summary)
-    return final_summary, summaries
+
+    return final_summary
+
+async def generate_compress_summary(script: str, mode: int):
+
+    max_cycle = int(len(script) / constants['chunk_size']['large']) + 1
+    summary_prompt = constants['prompt']['summary']['kr']
+    final_summary_prompt = constants['prompt']['final_summary']['en']
+    summary = ''
+
+    for cycle in range(max_cycle):
+
+        chunks = []
+
+        if mode == 1:
+            chunks = naive_divide(script)
+        elif mode == 2:
+            chunks = recursive_divide(script)
+        elif mode == 3:
+            chunks = spacy_divide(script)
+
+        if len(chunks) == 1:
+            summary = await request_gpt(script + final_summary_prompt,
+                                            constants['prompt']['final_summary']['system_message'])
+            break
+
+        tasks = [request_gpt(summary_prompt + "\n script : " + chunk,
+                                 constants['prompt']['summary']['system_message']) for idx, chunk in enumerate(chunks)]
+
+        chunk_summaries = await asyncio.gather(*tasks)
+        summary = ' '.join(chunk_summaries)
+
+        script = summary
+
+    return summary
 
 
-def naive_divide(scripts: dict):
+def naive_divide(script: str):
 
-    chunk_text = ''
-    time_stamp = 0
+    chunks = textwrap.wrap(script, 3000)
 
-    time_stamps = []
-    chunks = []
-    for script in scripts:
-        if len(chunk_text) > 3000:
-            chunk_text.replace("[음악]", "")
-            chunk_text.replace("[박수]", "")
-            chunks.append(chunk_text)
-            time_stamps.append(time_stamp)
-            time_stamp = script['start']
-            chunk_text = script['text'] + ' '
-        else:
-            chunk_text += script['text']
+    return chunks
 
-    if len(chunk_text) < 1000 and len(chunks) > 0:
-        chunks[-1] += chunk_text
-    else:
-        time_stamps.append(time_stamp)
-        chunks.append(chunk_text)
 
-    return time_stamps, chunks
+def recursive_divide(scripts: str):
 
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 3000, chunk_overlap = 50, length_function = len, is_separator_regex = False)
+    chunks = text_splitter.split_text(scripts)
+    print(chunks)
+
+    return chunks
+
+def spacy_divide(scripts: str):
+
+    text_splitter = SpacyTextSplitter(chunk_size = 3000, chunk_overlap = 50)
+    chunks = text_splitter.split_text(scripts)
+    print(chunks)
+
+    return chunks
 
 def reformat_summary(summary: str):
     summary.replace("\#", "#")
     return summary
+
+def preprocess_script(script: str):
+    script.replace("[음악]", "")
+    script.replace("[박수]", "")
+    script.replace("[환호]", "")
+
+    return script
